518d12bd463495384ce112e91f91942dc1306969
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-12-06 00:36:47 -0600

    - Appends table with data

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -7,3 +7,16 @@ var tbody = d3.select("tbody");
 
 //Console.log the weathr data from data.js
 console.log(data);
+
+//Use d3 to append 1 cell per sighting value
+
+data.forEach(function (sightings) {
+    //console.log(sightings);
+    var row = tbody.append("tr");
+    Object.entries(sightings).forEach(function ([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+
+    });
+});
+
