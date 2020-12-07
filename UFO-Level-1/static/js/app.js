07a52d7e14709dce56d5e8f48816bd3c84e3fd74
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-12-06 22:06:40 -0600

    - Using class code to filter data (not working)

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -20,3 +20,30 @@ data.forEach(function (sightings) {
     });
 });
 
+//Select the button
+var button = d3.select("#filter-btn");
+
+//Select the form
+var form = d3.select(".form-group");
+
+//Create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+//Complete the event handler function for the form
+function runEnter() {
+    //Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    //Select the input element and get the raw HTMl node
+    var inputElement = d3.select(datetime);
+
+    //Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter((date) => date.datetime === inputValue);
+    console.log(filteredData);
+}
+
