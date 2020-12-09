fb504d10dc20ff16b03c57b2a3ed57d43302a5da
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-12-08 22:33:17 -0600

    - Removes test code not needed for functionality

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -5,11 +5,10 @@ var tableData = data;
 //Get a reference to the table body
 var tbody = d3.select("tbody");
 
-//Console.log the weathr data from data.js
+//Console.log the weathwr data from data.js
 console.log(data);
 
 //Use d3 to append 1 cell per sighting value
-
 data.forEach(function (sightings) {
     //console.log(sightings);
     var row = tbody.append("tr");
@@ -20,12 +19,10 @@ data.forEach(function (sightings) {
     });
 });
 
-//Select the button
+//Select the filter button
 var button = d3.select("#filter-btn");
 
 //Select the form
-//var form = d3.select(".form-group");
-//var form = d3.select("#form");
 var form = d3.select(".form-control");
 
 //Create event handlers
@@ -44,21 +41,12 @@ function runEnter() {
     //Get the value property of the input element
     var inputValue = inputElement.property("value");
     
-    //console.log(inputValue);
-    //console.log(tableData);
-
-    //var filteredData = tableData.filter((date) => date.datetime === inputValue);
-
+    
     var filteredData = tableData.filter(date => date.datetime === inputValue);
-    //console.log(filteredData);
-    //return(filteredData);
-
-
+   
     /////////////////////////////////////////////////
 
-
     //Select Unordered list element by ID
-    //var list = d3.select(".list-group");
     var list = d3.select("#filters");
 
     //Remove all data from the table to replace with filtered data (Lesson 3, Activity 9)
@@ -67,7 +55,7 @@ function runEnter() {
     //Use function similar to above to display filteredData table
 
     filteredData.forEach(function (byDate) {
-        //console.log(sightings);
+        
         var row = tbody.append("tr");
         Object.entries(byDate).forEach(function ([key, value]) {
             var cell = row.append("td");
