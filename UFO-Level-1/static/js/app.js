c3f147611fbdbdf248a73d53d6341240ca96491e
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-12-12 15:03:40 -0600

    - Cleaning up unnecessary code for Level 1

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -22,12 +22,9 @@ data.forEach(function (sightings) {
 //Select the filter button
 var button = d3.select("#filter-btn");
 
-//Select the form
-var form = d3.select(".form-control");
-
 //Create event handlers
 button.on("click", runEnter);
-form.on("submit", runEnter);
+
 
 //Complete the event handler function for the form
 function runEnter() {
